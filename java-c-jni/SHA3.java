5634e8ef2d331145e13da80af0bf31f3c01f3add
Mattias Andrée <maandree@operamail.com>
2013-02-09 06:30:01 +0100

    beginning jni implemenetation
    Signed-off-by: Mattias Andrée <maandree@operamail.com>

---
+++ java-c-jni/SHA3.java
@@ -0,0 +1,106 @@
+/**
+ * sha3sum – SHA-3 (Keccak) checksum calculator
+ * 
+ * Copyright © 2013  Mattias Andrée (maandree@member.fsf.org)
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+/**
+ * SHA-3/Keccak hash algorithm implementation
+ * 
+ * @author  Mattias Andrée  <a href="mailto:maandree@member.fsf.org">maandree@member.fsf.org</a>
+ */
+public class SHA3
+{
+    /**
+     * Hidden constructor
+     */
+    private SHA3()
+    {
+	// Inhibit instansiation
+    }
+    
+
+    
+    /**
+     * Class initialiser
+     */
+    static
+    {
+	/* TODO load lib here */
+    }
+    
+    
+    
+    /**
+     * Initialise Keccak sponge
+     * 
+     * @param  r  The bitrate
+     * @param  c  The capacity
+     * @param  n  The output size
+     */
+    public static native void initialise(int r, int c, int n);
+    
+    
+    /**
+     * Absorb the more of the message message to the Keccak sponge
+     * 
+     * @param  msg  The partial message
+     */
+    public static void update(byte[] msg)
+    {
+	update(msg, msg.length);
+    }
+    
+    
+    /**
+     * Absorb the more of the message message to the Keccak sponge
+     * 
+     * @param  msg     The partial message
+     * @param  msglen  The length of the partial message
+     */
+    public static native void update(byte[] msg, int msglen);
+    
+    
+    /**
+     * Squeeze the Keccak sponge
+     */
+    public static byte[] digest()
+    {
+	return digest(null);
+    }
+    
+    
+    /**
+     * Absorb the last part of the message and squeeze the Keccak sponge
+     * 
+     * @param  msg  The rest of the message
+     */
+    public static byte[] digest(byte[] msg)
+    {
+	return digest(msg, msg == null ? 0 : msg.length);
+    }
+    
+    
+    /**
+     * Absorb the last part of the message and squeeze the Keccak sponge
+     * 
+     * @param  msg     The rest of the message
+     * @param  msglen  The length of the partial message
+     */
+    public static native byte[] digest(byte[] msg, int msglen);
+    
+}
